3caea5c4f8079116da54649c47727a16927533f2
zxc58 <abc01234567889@gmail.com>
2022-12-18 18:53:46 +0800

    create model "calendar"

--- models/calendar.js
+++ models/calendar.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Calendar extends Model {
     /**
@@ -9,18 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
   Calendar.init({
-    user_id: DataTypes.INTEGER,
     date: DataTypes.DATE,
     day: DataTypes.STRING,
     is_holiday: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Calendar',
-  });
-  return Calendar;
-};
+    tableName: 'Calendar',
+    underscored: true
+  })
+  return Calendar
+}
