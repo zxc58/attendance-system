666f1fa5039cb5b25026ac11dbe5fd2183a8b724
zxc58 <abc01234567889@gmail.com>
2023-01-01 02:44:43 +0800

    set token's age

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -3,8 +3,8 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET
 
 exports.signJWT = ({ res, user }) => {
-  const accessToken = jwt.sign({ user, type: 'access_token' }, accessTokenSecret, { expiresIn: '2h' })
+  const accessToken = jwt.sign({ user, type: 'access_token' }, accessTokenSecret, { expiresIn: '5m' })
   const newRefreshToken = jwt.sign({ type: 'refresh_token', userId: user.id }, refreshTokenSecret)
-  res.cookie('refresh_token', newRefreshToken, { httpOnly: true, signed: true })
+  res.cookie('refresh_token', newRefreshToken, { httpOnly: true, signed: true, maxAge: 3600 * 1000 })
   return res.json({ message: 'Get jwt', accessToken })
 }
