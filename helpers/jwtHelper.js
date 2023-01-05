ff8571543556be91e5dd2929d2ef36ee036b493c
zxc58 <abc01234567889@gmail.com>
2023-01-05 15:36:17 +0800

    update ENV_VAR and ENV_DOC

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -2,15 +2,15 @@ const jwt = require('jsonwebtoken')
 const { momentTW } = require('./timeHelper')
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET
-const accessTokenMaxage = process.env.ACCESS_TOKEN_MAXAGE ?? 30000
-const refreshTokenMaxage = process.env.REFRESH_TOKEN_MAXAGE ?? 50000
+const accessTokenMaxage = Number(process.env.ACCESS_TOKEN_MAXAGE ?? 30000)
+const refreshTokenMaxage = Number(process.env.REFRESH_TOKEN_MAXAGE ?? 50000)
 exports.signJWT = ({ res, user, getRefreshToken }) => {
   const accessToken = jwt.sign(
     { user, type: 'access_token' },
     accessTokenSecret,
     { expiresIn: `${accessTokenMaxage}s` }
   )
-  const accessTokenExpiredTime = momentTW().add(50, 's').toDate()
+  const accessTokenExpiredTime = momentTW().add(accessTokenMaxage - 10, 's').toDate()
   const responseData = { userId: user.id, accessToken, accessTokenExpiredTime }
   if (getRefreshToken) {
     const refreshToken = jwt.sign(
