65d71da720b8f502c4c6012a35f197ef5f3b980a
zxc58 <abc01234567889@gmail.com>
2023-01-06 22:16:28 +0800

    add attendance seed

--- seeders/20221218141913-employee-seeder.js
+++ seeders/20221218141913-employee-seeder.js
@@ -7,7 +7,7 @@ module.exports = {
     const queryTypes = { type: queryInterface.sequelize.QueryTypes.SELECT }
     const [dates, departments] = await Promise.all([
       queryInterface.sequelize.query(
-        "SELECT id,date from titansoft.calendar where date='2023-01-01'"
+        "SELECT id,date from titansoft.calendar where date='2022-10-12'"
         , queryTypes
       ),
       queryInterface.sequelize.query(
